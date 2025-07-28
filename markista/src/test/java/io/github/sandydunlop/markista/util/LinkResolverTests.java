166fb51c0e6851f421fcf53a56098db959c2cf1b
Sandy Dunlop <sandy.dunlop@gmail.com>
2025-07-28 13:17:58 +0100

    Squashed commit of the following:
    commit 326ad5af1be50cf6c68e148a840f064da11ff7b1
    Author: Sandy Dunlop <sandy.dunlop@gmail.com>
    Date:   Sun Jul 27 08:39:30 2025 +0100
    
        updated link to javadocs
    
    commit 242054b786b2bb1c3e74d316a1e6b5c193c51d94
    Author: Sandy Dunlop <sandy.dunlop@gmail.com>
    Date:   Sun Jul 27 08:16:31 2025 +0100
    
        moving src into individual folder for module, specifying built-in module list in code instead of resources to save having to configure class path for doclet
    
    commit 72e35f2e3aa2adc6f7fb6803d9ca2fefeaa33923
    Author: Sandy Dunlop <sandy.dunlop@gmail.com>
    Date:   Sat Jul 26 18:16:17 2025 +0100
    
        Squashed commit of the following:
    
        commit 83a65163f4d8268d862022f95f01ce067bb2f1b0
        Author: Sandy Dunlop <sandy.dunlop@gmail.com>
        Date:   Sat Jul 26 18:15:47 2025 +0100
    
            version 0.1.5
    
        commit ab07158ab6cfa59afee4acc38e8dd2a5c7d884f4
        Author: Sandy Dunlop <sandy.dunlop@gmail.com>
        Date:   Sat Jul 26 17:41:20 2025 +0100
    
            Test to ensure resolver doesn't get confused by wrong package names being used to qualify classes
    
        commit e87040e0dc4ccad26d20bf5ff5229e2004bf3ec1
        Author: Sandy Dunlop <sandy.dunlop@gmail.com>
        Date:   Sat Jul 26 17:33:07 2025 +0100
    
            linking between different modules
    
        commit 22d5b2e27c2af429ddb86ad376e9f1a71439def2
        Author: Sandy Dunlop <sandy.dunlop@gmail.com>
        Date:   Sat Jul 26 15:07:34 2025 +0100
    
            More module info
    
        commit 531f18328dab7abfc0f9c8d218818ebbb78b9510
        Author: Sandy Dunlop <sandy.dunlop@gmail.com>
        Date:   Sat Jul 26 01:21:33 2025 +0100
    
            Major refactor including removing all references to JDK and javax types from model package
    
        commit 743e2aeb8ff07812c6ab878523cf8248ba3c3ab0
        Author: Sandy Dunlop <sandy.dunlop@gmail.com>
        Date:   Sat Jul 26 00:36:53 2025 +0100
    
            Refactor
    
        commit 3a8d23ccfc70fa09b6950e7808781040e5276f25
        Author: Sandy Dunlop <sandy.dunlop@gmail.com>
        Date:   Fri Jul 25 21:52:43 2025 +0100
    
            mainly expanding tests

---
+++ markista/src/test/java/io/github/sandydunlop/markista/util/LinkResolverTests.java
@@ -0,0 +1,178 @@
+package io.github.sandydunlop.markista.util;
+
+import com.sun.source.util.DocTreePath;
+
+import io.github.sandydunlop.markista.model.Api;
+import io.github.sandydunlop.markista.model.PackageNode;
+import io.github.sandydunlop.markista.model.Reference;
+import io.github.sandydunlop.markista.model.ClassNode;
+import io.github.sandydunlop.markista.model.ModuleNode;
+
+import javax.lang.model.element.Element;
+import javax.tools.Diagnostic.Kind;
+
+import jdk.javadoc.doclet.Reporter;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class LinkResolverTests {
+	private Api api;
+    private ModuleNode module;
+    private PackageNode model;
+    private PackageNode doclet;
+	private PackageNode markista;
+	private PackageNode util;
+    private ClassNode node;
+    private ClassNode markdownDoclet;
+
+	@Mock static Reporter reporter = new Reporter() {
+        @Override
+        public void print(Kind kind, String message) {
+            System.out.println(kind + ": " + message);
+        }
+
+        @Override
+        public void print(Kind kind, DocTreePath path, String message) {
+            // Do nothing
+        }
+
+        @Override
+        public void print(Kind kind, Element element, String message) {
+            // Do nothing
+        }
+    };
+
+	@BeforeAll
+    static void initAll() {
+		Configuration.setReporter(reporter);
+        LinkResolver.addNativeModule("java.base", "https://docs.oracle.com/en/java/javase/24/docs/api/java.base", ".html");
+    }
+
+    @BeforeEach
+    void init() {
+		api = new Api();
+        api.addPackage(new PackageNode("io.github.sandydunlop"));
+        markista = new PackageNode("io.github.sandydunlop.markista");
+		util = new PackageNode("io.github.sandydunlop.markista.util");
+		doclet = new PackageNode("io.github.sandydunlop.markista.doclet");
+		model = new PackageNode("io.github.sandydunlop.markista.model");
+		api.addPackage(markista);
+		api.addPackage(util);
+		api.addPackage(doclet);
+		api.addPackage(model);
+		api.addClass(new ClassNode("io.github.sandydunlop.markista.util.LinkResolver","LinkResolver", util));
+		api.addClass(new ClassNode("io.github.sandydunlop.markista.doclet.MarkdownDoclet","MarkdownDoclet", doclet));
+		api.addClass(new ClassNode("io.github.sandydunlop.markista.doclet.MarkdownDoclet.Option","MarkdownDoclet.Option", doclet));
+
+        module = new ModuleNode("sandydunlop.markista");
+		api.addModule(module);
+		module.addPackage(markista);
+		module.addPackage(util);
+		module.addPackage(doclet);
+		module.addPackage(model);
+		markista.setModule(module);
+		util.setModule(module);
+		doclet.setModule(module);
+		model.setModule(module);
+
+        node = new ClassNode("io.github.sandydunlop.markista.model.Node", "Node", model);
+        model.addClass(node);
+        api.addClass(node);
+
+        markdownDoclet = new ClassNode("io.github.sandydunlop.markista.doclet.MarkdownDoclet", "MarkdownDoclet", doclet);
+        model.addClass(markdownDoclet);
+
+
+		LinkResolver.setCurrentModuleName("sandydunlop.markista");
+		LinkResolver.setApi(api);
+		LinkResolver.setFlattenedDirectories(null);
+	}
+
+	@Test
+	void resolve_qualifiedNativeClass() {
+		Reference link = LinkResolver.resolve("java.util.List");
+		assertEquals("https://docs.oracle.com/en/java/javase/24/docs/api/java.base/java/util/List.html", link.getUri());
+	}
+
+	@Test
+	void resolve_unqualifiedPackage_nextLevelPackage() {
+		Reference link = LinkResolver.resolve("io.github.sandydunlop.markista", "doclet");
+		assertEquals("doclet", link.getUri());
+	}
+
+	@Test
+	void resolve_unqualifiedPackage_sameLevelPackage() {
+		Reference link = LinkResolver.resolve("io.github.sandydunlop.markista.util", "doclet");
+		assertEquals("../doclet", link.getUri());
+	}
+
+	@Test
+	void resolve_unqualifiedClass_sameLevel() {
+		Reference link = LinkResolver.resolve("io.github.sandydunlop.markista.util", "MarkdownDoclet");
+		assertEquals("../doclet/MarkdownDoclet", link.getUri());
+	}
+
+	@Test
+	void resolve_qualifiedClass_noLevel() {
+		Reference link = LinkResolver.resolve("io.github.sandydunlop.markista.model.Node");
+		assertEquals("io/github/sandydunlop/markista/model/Node", link.getUri());
+	}
+
+	@Test
+	void resolve_unqualifiedPackage_prevLevel() {
+		Reference link = LinkResolver.resolve("io.github.sandydunlop.markista.util", "markista");
+		assertEquals("..", link.getUri());
+	}
+
+	@Test
+	void resolve_qualifiedPackage_prevLevel() {
+		Reference link = LinkResolver.resolve("io.github.sandydunlop.markista.util", "io.github.sandydunlop.markista");
+		assertEquals("..", link.getUri());
+	}
+
+	@Test
+	void resolve_primitive() {
+		Reference link = LinkResolver.resolve("io.github.sandydunlop.markista.util", "boolean");
+		assertEquals(Reference.Kind.PRIMITIVE, link.getKind());
+	}
+
+	@Test
+	void relativize_toParentLevel() {
+		String link = LinkResolver.relativize("io.github.sandydunlop.markista.util", "io.github.sandydunlop.markista");
+		assertEquals("..", link);
+	}
+
+	@Test
+	void relativize_toSameLevel() {
+		String link = LinkResolver.relativize("io.github.sandydunlop.markista.util", "io.github.sandydunlop.markista.doclet");
+		assertEquals("../doclet", link);
+	}
+
+	@Test
+	void relativize_fromNoLevel() {
+		String path = LinkResolver.relativize("", "io.github.sandydunlop.markista.model.Node");
+		assertEquals("io/github/sandydunlop/markista/model/Node", path);
+	}
+
+	@Test
+	void relativize_squashed_toSameLevel() {
+		LinkResolver.setFlattenedDirectories("io.github.sandydunlop.markista");
+		String path = LinkResolver.relativize("io.github.sandydunlop.markista.util", "io.github.sandydunlop.markista.doclet");
+		assertEquals("../doclet", path);
+	}
+
+	@Test
+	void relativize_squashed_fromNoLevel() {
+		LinkResolver.setFlattenedDirectories("io.github.sandydunlop.markista");
+		String path = LinkResolver.relativize("", "io.github.sandydunlop.markista.doclet");
+		assertEquals("doclet", path);
+	}
+}
---
+++ markista/src/test/java/io/github/sandydunlop/markista/util/MarkdownTests.java
@@ -0,0 +1,197 @@
+package io.github.sandydunlop.markista.util;
+
+import com.sun.source.util.DocTreePath;
+
+import io.github.sandydunlop.markista.model.Api;
+import io.github.sandydunlop.markista.model.ClassNode;
+import io.github.sandydunlop.markista.model.MethodNode;
+import io.github.sandydunlop.markista.model.ModuleNode;
+import io.github.sandydunlop.markista.model.PackageNode;
+import io.github.sandydunlop.markista.model.ParamNode;
+import io.github.sandydunlop.markista.model.Reference;
+import io.github.sandydunlop.markista.model.TypeNode;
+import jdk.javadoc.doclet.Reporter;
+
+import javax.lang.model.element.Element;
+import javax.tools.Diagnostic.Kind;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class MarkdownTests {
+	private Api api;
+    private ModuleNode module;
+    private PackageNode model;
+    private PackageNode doclet;
+	private PackageNode markista;
+	private PackageNode util;
+    private ClassNode node;
+    private ClassNode markdownDoclet;
+
+    @Mock static Reporter reporter = new Reporter() {
+        @Override
+        public void print(Kind kind, String message) {
+            System.out.println(kind + ": " + message);
+        }
+
+        @Override
+        public void print(Kind kind, DocTreePath path, String message) {
+            // Do nothing
+        }
+
+        @Override
+        public void print(Kind kind, Element element, String message) {
+            // Do nothing
+        }
+    };
+    
+    @BeforeAll
+    static void initAll() {
+		Configuration.setReporter(reporter);
+        LinkResolver.addNativeModule("java.base", "https://docs.oracle.com/en/java/javase/24/docs/api/java.base", ".html");
+    }
+
+    @BeforeEach
+    void init() {
+		api = new Api();
+        api.addPackage(new PackageNode("io.github.sandydunlop"));
+        markista = new PackageNode("io.github.sandydunlop.markista");
+		util = new PackageNode("io.github.sandydunlop.markista.util");
+		doclet = new PackageNode("io.github.sandydunlop.markista.doclet");
+		model = new PackageNode("io.github.sandydunlop.markista.model");
+		api.addPackage(markista);
+		api.addPackage(util);
+		api.addPackage(doclet);
+		api.addPackage(model);
+		api.addClass(new ClassNode("io.github.sandydunlop.markista.util.LinkResolver","LinkResolver", util));
+		api.addClass(new ClassNode("io.github.sandydunlop.markista.doclet.MarkdownDoclet","MarkdownDoclet", doclet));
+		api.addClass(new ClassNode("io.github.sandydunlop.markista.doclet.MarkdownDoclet.Option","MarkdownDoclet.Option", doclet));
+
+        module = new ModuleNode("sandydunlop.markista");
+		api.addModule(module);
+		module.addPackage(markista);
+		module.addPackage(util);
+		module.addPackage(doclet);
+		module.addPackage(model);
+		markista.setModule(module);
+		util.setModule(module);
+		doclet.setModule(module);
+		model.setModule(module);
+
+        node = new ClassNode("io.github.sandydunlop.markista.model.Node", "Node", model);
+        model.addClass(node);
+        api.addClass(node);
+
+        markdownDoclet = new ClassNode("io.github.sandydunlop.markista.doclet.MarkdownDoclet", "MarkdownDoclet", doclet);
+        model.addClass(markdownDoclet);
+
+		LinkResolver.setCurrentModuleName("sandydunlop.markista");
+        LinkResolver.setApi(api);
+		LinkResolver.setFlattenedDirectories(null);
+        LinkResolver.setCurrentPackageName("io.github.sandydunlop.markista.doclet");
+    }
+
+    @Test
+    void fullSignature() {
+        MethodNode method = new MethodNode(node, "subject");
+        TypeNode param1type = new TypeNode("java.lang.String", "String", model);
+        method.addParam(new ParamNode(param1type, "name"));
+        String sig = Markdown.fullSignature(method);
+        assertEquals("[Node](../model/Node.md) subject([String](https://docs.oracle.com/en/java/javase/24/docs/api/java.base/java/lang/String.html) name)", sig);
+    }
+
+    @Test
+    void formatParams() {
+        List<ParamNode> params = new ArrayList<>();
+        TypeNode param1type = new TypeNode("java.lang.String", "String", model);
+        params.add(new ParamNode(param1type, "name"));
+        String markdown = Markdown.formatParams(params);
+        assertEquals("[String](https://docs.oracle.com/en/java/javase/24/docs/api/java.base/java/lang/String.html) name", markdown);
+    }
+
+    @Test
+    void formatReference_URL() {
+        Reference ref = new Reference(Reference.Kind.URL, "name", "http://example.com");
+        String markdown = Markdown.formatReference(ref);
+        assertEquals("[http://example.com](http://example.com)", markdown);
+    }
+
+    @Test
+    void formatReference_PACKAGE_qualified() {
+        Reference ref = new Reference(Reference.Kind.PACKAGE, "io.github.sandydunlop.markista.model", null);
+        String markdown = Markdown.formatReference(ref);
+        assertEquals("[io.github.sandydunlop.markista.model](../model/index.md)", markdown);
+    }
+
+    @Test
+    void formatReference_PACKAGE_unqualified() {
+        Reference ref = new Reference(Reference.Kind.PACKAGE, "model", null);
+        String markdown = Markdown.formatReference(ref);
+        assertEquals("[model](../model/index.md)", markdown);
+    }
+
+    @Test
+    void formatReference_TYPE_qualified_wrongPackage() {
+        Reference ref = new Reference(Reference.Kind.TYPE, "io.github.sandydunlop.model.Node", null);
+        String markdown = Markdown.formatReference(ref);
+        assertEquals("Node", markdown);
+    }
+
+    @Test
+    void formatReference_TYPE_qualified() {
+        Reference ref = new Reference(Reference.Kind.TYPE, "io.github.sandydunlop.markista.model.Node", null);
+        String markdown = Markdown.formatReference(ref);
+        assertEquals("[Node](../model/Node.md)", markdown);
+    }
+
+    @Test
+    void formatReference_TYPE_unqualified() {
+        Reference ref = new Reference(Reference.Kind.TYPE, "Node", null);
+        String markdown = Markdown.formatReference(ref);
+        assertEquals("[Node](../model/Node.md)", markdown);
+    }
+
+    @Test
+    void formatReference_PAGE_withTitle() {
+        Reference ref = new Reference(Reference.Kind.PAGE, "Page", "page");
+        String markdown = Markdown.formatReference(ref);
+        assertEquals("[Page](../../../../../page.md)", markdown);
+    }
+
+    @Test
+    void formatReference_PAGE_withoutTitle() {
+        Reference ref = new Reference(Reference.Kind.PAGE, "", "page");
+        String markdown = Markdown.formatReference(ref);
+        assertEquals("[page](../../../../../page.md)", markdown);
+    }
+
+    @Test
+    void formatReference_PRIMITIVE() {
+        // This should never be created, so its value here shouldn't matter
+        Reference ref = new Reference(Reference.Kind.PRIMITIVE, "int", null);
+        String markdown = Markdown.formatReference(ref);
+        assertEquals("", markdown);
+    }
+
+	@Test
+	void link_qualifiedWithAnchor() {
+        LinkResolver.setCurrentPackageName("io.github.sandydunlop.markista.doclet.MarkdownDoclet");
+        String markdown = Markdown.mdAutoLink("io.github.sandydunlop.markista.util.Markdown#mdAutoLink", false);
+        assertEquals("[mdautolink](../util/Markdown.md#mdautolink)", markdown);
+    }
+
+    // @Test
+    // void formatTaggedText() {
+    //     List<DocTree> 
+    // }
+}
