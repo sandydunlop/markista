110403089ead95321694b99cf1b9d271755d6328
Sandy Dunlop <sandy.dunlop@gmail.com>
2025-08-15 22:19:47 +0100

    r0.1.16

--- markista/src/main/java/io/github/sandydunlop/markista/core/Context.java
+++ markista/src/main/java/io/github/sandydunlop/markista/core/Context.java
@@ -276,7 +276,8 @@ public class Context { //NOSONAR - This works best as a singleton but Sonar show
         File containingDir = getPackageDirectory();
         if (!containingDir.exists()) containingDir.mkdirs();
         if (typeName.isEmpty()) typeName = "index";
-        return new File(containingDir, typeName + ".md");
+        String fileName = NameSimplifier.simplifyNames(typeName);
+        return new File(containingDir, fileName + ".md");
     }
 
     /// Creates a Writer for writing a module-level file inside the module's directory.
@@ -317,5 +318,75 @@ public class Context { //NOSONAR - This works best as a singleton but Sonar show
     private char pathSeparator() {
         // File.pathSeparatorChar is returning ":" on macOS (Sequoia 15.5) when it should be "/"
         return File.pathSeparatorChar == ':' ? '/' : File.pathSeparatorChar;
+    }
+
+    public class NameSimplifier {
+        private NameSimplifier() {
+            // Nothing to see here
+        }
+
+        /// Changes all qualified names in a string into unqualified names.
+        /// @param  str A string that may contain one or more qualified names.
+        /// @return The input string, with all qualified names changed to unqualified names.
+        public static String simplifyNames(String str) {
+            if (str == null || str.isEmpty())
+                return "";
+            return simplifyNamesLoop(str);
+        }
+
+        private static String simplifyNamesLoop(String input) {
+            String simplified = input;
+            int qualifiedStart = -1;
+            int simpleStart = -1;
+            char prev = (char) 0;
+            int i = 0;
+            while (i <= simplified.length()) {
+                char c = i < simplified.length() ? simplified.charAt(i) : ' ';
+                if (shouldReplaceQualifiedWithSimple(qualifiedStart, simpleStart, i, simplified.length(), c)) {
+                    simplified = replaceQualifiedWithSimple(simplified, qualifiedStart, simpleStart, i);
+                    i = qualifiedStart + (i - simpleStart);
+                    simpleStart = -1;
+                    qualifiedStart = -1;
+                } else if (qualifiedStart == -1 && isValidQualifiedNameChar(c) && !isValidSimpleNameChar(prev)) {
+                    qualifiedStart = i;
+                } else if (qualifiedStart > -1 && simpleStart == -1 && Character.isUpperCase(c)) {
+                    simpleStart = i;
+                } else if (qualifiedStart > -1 && simpleStart == -1 && !isValidQualifiedNameChar(c)) {
+                    qualifiedStart = -1;
+                }
+                prev = c;
+                i++;
+            }
+            return simplified;
+        }
+
+        private static boolean shouldReplaceQualifiedWithSimple(int qualifiedStart, int simpleStart, int i, int length,
+                char c) {
+            return qualifiedStart > -1 && simpleStart > -1 && (i == length || !isValidSimpleNameChar(c));
+        }
+
+        private static String replaceQualifiedWithSimple(String simplified, int qualifiedStart, int simpleStart, int i) {
+            StringBuilder tmp = new StringBuilder();
+            if (qualifiedStart > 0)
+                tmp.append(simplified, 0, qualifiedStart);
+            tmp.append(simplified.substring(simpleStart, i));
+            if (i < simplified.length())
+                tmp.append(simplified.substring(i));
+            return tmp.toString();
+        }
+
+        /// Checks if the given character is valid in an unqualified name.
+        /// @param c The character to check.
+        /// @return  Whether or not the character is valid in an unqualified name.
+        public static boolean isValidSimpleNameChar(char c) {
+            return Character.isAlphabetic(c) || c == '.';
+        }
+
+        /// Checks if the given character is valid in a qualified name.
+        /// @param c The character to check.
+        /// @return  Whether or not the character is valid in a qualified name.
+        public static boolean isValidQualifiedNameChar(char c) {
+            return (Character.isAlphabetic(c) && Character.isLowerCase(c)) || c == '.';
+        }
     }
 }
