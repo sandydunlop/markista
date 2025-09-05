150db1c1f5bcc7311315b7e6ce501370e95bd567
Sandy Dunlop <sandy.dunlop@gmail.com>
2025-09-05 18:05:43 +0100

    r0.2.2

--- markista/src/main/java/io/github/sandydunlop/markista/orchestration/LinkResolver.java
+++ markista/src/main/java/io/github/sandydunlop/markista/orchestration/LinkResolver.java
@@ -117,10 +117,6 @@ public class LinkResolver {
         if (link == null || link.getTarget() == null || link.getTarget().isEmpty() || link.isResolved()) {
             return false;
         }
-        if (link.getTarget().equals("jdk.javadoc.doclet.Doclet.Option.Kind")) {
-            link=link;
-
-        }
         if (preResolve(link)) return true;
         if (resolvePrimitiveOrVoid(link)) return true;
         if (resolveLocalPackageOrType(link)) return true;
